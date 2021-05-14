a1c22edfb76472c5982f875a247f4823ef249708
Georg Steltner <georg.steltner@gmail.com>
2021-05-14 16:53:39 +0200

    Add new dev mode for DatabaseFactory

--- src/Service/DatabaseFactory.php
+++ src/Service/DatabaseFactory.php
@@ -3,6 +3,7 @@
 namespace Easy\Service;
 
 use PDO;
+use PDOException;
 use Psr\Container\ContainerInterface;
 
 use function current;
@@ -12,6 +13,9 @@ class DatabaseFactory
     public function __invoke(ContainerInterface $container): PDO
     {
         $settings = $container->get('config');
+
+        $debug = $settings['debug'];
+
         $settings = $settings['database'];
         $settings = current($settings);
 
@@ -19,6 +23,18 @@ class DatabaseFactory
         $user = $settings['user'];
         $password = $settings['password'];
 
-        return new PDO($dsn, $user, $password, [PDO::ATTR_ERRMODE => $settings['error'], PDO::ATTR_EMULATE_PREPARES => false]);
+        try {
+            $pdo = new PDO($dsn, $user, $password, [PDO::ATTR_ERRMODE => $settings['error'], PDO::ATTR_EMULATE_PREPARES => false]);
+        } catch (PDOException $e) {
+            if (!$debug || $e->getCode() !== 1049) {
+                throw $e;
+            }
+
+            $dsn = 'mysql:dbname=mysql;host=' . $settings['host'] . ';charset=utf8';
+
+            $pdo = new PDO($dsn, $user, $password, [PDO::ATTR_ERRMODE => $settings['error'], PDO::ATTR_EMULATE_PREPARES => false]);
+        }
+
+        return $pdo;
     }
 }
