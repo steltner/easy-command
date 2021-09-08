9b4b018387a64eb7566067e045cea76d74459907
Georg Steltner <georg.steltner@gmail.com>
2021-09-08 02:03:56 +0200

    Add option to CodeSnifferCommand to show related sniff rules

--- src/Command/CodeSnifferCommand.php
+++ src/Command/CodeSnifferCommand.php
@@ -6,6 +6,8 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
+use const DS;
+
 class CodeSnifferCommand extends AbstractCommand
 {
     protected function configure(): void
@@ -15,20 +17,26 @@ class CodeSnifferCommand extends AbstractCommand
             ->setDescription('Run php code sniffer')
             ->setHelp('Run php code sniffer')
             ->setAliases(['cs', 'phpcs'])
+            ->addOption('sniffs', 's', InputOption::VALUE_NONE, 'Show sniff rules')
             ->addOption('project', 'p', InputOption::VALUE_OPTIONAL, 'Project name or short tag');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $project = $input->getOption('project');
+        $sniffs = $input->getOption('sniffs');
 
         $project = $this->getProjectByName($project);
 
         $projectPath = $this->getFilePath($project['path']);
 
-        $this->executeCommand(
-            ['php', $projectPath . 'vendor' . DS . 'squizlabs' . DS . 'php_codesniffer' . DS . 'bin' . DS . 'phpcs']
-        );
+        $command = ['php', $projectPath . 'vendor' . DS . 'squizlabs' . DS . 'php_codesniffer' . DS . 'bin' . DS . 'phpcs'];
+
+        if ($sniffs) {
+            $command[] = '-s';
+        }
+
+        $this->executeCommand($command);
 
         return 0;
     }
